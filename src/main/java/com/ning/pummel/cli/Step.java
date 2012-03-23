073bb32fff29e60e17153445b039f4b6cc66e38d
Brian McCallister <brianm@skife.org>
2012-03-23 16:37:32 -0600

    support clojure for step functions

--- src/main/java/com/ning/pummel/cli/Step.java
+++ src/main/java/com/ning/pummel/cli/Step.java
@@ -1,18 +1,23 @@
 package com.ning.pummel.cli;
 
+import clojure.lang.IFn;
+import clojure.lang.RT;
+import com.google.common.base.Optional;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Lists;
+import com.ning.pummel.Fight;
 import org.apache.commons.math.stat.descriptive.DescriptiveStatistics;
 import org.mvel.MVEL;
 import org.skife.cli.Arguments;
 import org.skife.cli.Command;
 import org.skife.cli.Option;
-import com.ning.pummel.Fight;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
+import java.io.Serializable;
+import java.io.StringReader;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.Callable;
@@ -64,6 +69,18 @@ public class Step implements Callable<Void>
         }
 
 
+        Optional<StepFunction> of = mvel(stepFunction);
+        if (!of.isPresent()) {
+            of = clojure(stepFunction);
+        }
+
+        if (!of.isPresent()) {
+            System.err.printf("'%s' is an invalid step function\n", stepFunction);
+            return null;
+        }
+
+        StepFunction step = of.get();
+
         if (labels) {System.out.printf("clients\ttp%.1f\tmean\treqs/sec\n", percentile);}
         int concurrency = start;
         do {
@@ -74,9 +91,55 @@ public class Step implements Callable<Void>
                               stats.getMean(),
                               (1000 / stats.getMean()) * concurrency);
             Map<String, Integer> vals = ImmutableMap.of("c", concurrency);
-            concurrency = (Integer) MVEL.eval(stepFunction, vals);
+            concurrency = step.step(concurrency);
         }
         while (concurrency < limit);
         return null;
+    }
+
+    private static interface StepFunction
+    {
+        public int step(int from);
+    }
+
+
+    private static Optional<StepFunction> clojure(String src) throws ClassNotFoundException
+    {
+        Class.forName(RT.class.getName());
+        try {
+            final IFn fn = (IFn) clojure.lang.Compiler.load(new StringReader(src));
+            StepFunction step = new StepFunction()
+            {
+                public int step(int from)
+                {
+                    return ((Long)fn.invoke(from)).intValue();
+                }
+            };
+            return Optional.of(step);
+
+        }
+        catch (Exception e) {
+            return Optional.absent();
+        }
+
+    }
+
+    private static Optional<StepFunction> mvel(String source)
+    {
+        try {
+            final Serializable mvel = MVEL.compileExpression(source);
+            MVEL.executeExpression(mvel, ImmutableMap.of("c", 1));
+            StepFunction step = new StepFunction()
+            {
+                public int step(int from)
+                {
+                    return (Integer) MVEL.executeExpression(mvel, ImmutableMap.of("c", from));
+                }
+            };
+            return Optional.of(step);
+        }
+        catch (Exception e) {
+            return Optional.absent();
+        }
     }
 }
