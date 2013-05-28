430100b4e4ee55e112ff8efe02bded579a13c2d5
Brian McCallister <brianm@skife.org>
2013-05-28 13:19:53 -0600

    support post mostly;

--- src/main/java/com/ning/pummel/Fist.java
+++ src/main/java/com/ning/pummel/Fist.java
@@ -1,42 +1,59 @@
 package com.ning.pummel;
 
-import com.google.common.io.ByteProcessor;
-import com.google.common.io.ByteStreams;
-import com.google.common.io.Resources;
+import com.squareup.okhttp.OkHttpClient;
 
-import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.Callable;
 import java.util.concurrent.TimeUnit;
 
 public class Fist implements Callable<Poll>
 {
-    private final String     url;
+    private static final OkHttpClient http = new OkHttpClient();
+    private final URL url;
+    private final byte[] body;
 
-    public Fist(String url)
+    public Fist(String url) throws MalformedURLException
     {
-        this.url = url;
+        String[] parts = url.split("\\s+");
+        this.url = new URL(parts[0]);
+
+        if (parts.length == 1) {
+            body = null;
+        }
+        else {
+            body = parts[1].getBytes();
+        }
+
     }
 
     public Poll call() throws Exception
     {
         long start = System.nanoTime();
-        ByteStreams.readBytes(Resources.newInputStreamSupplier(new URL(this.url)), new ByteProcessor<Object>()
-        {
-            public boolean processBytes(byte[] buf, int off, int len) throws IOException
-            {
-                return true;
-            }
+        HttpURLConnection conn = http.open(this.url);
+        if (body != null) {
+            conn.setRequestMethod("POST");
 
-            public Object getResult()
-            {
-                return null;
-            }
-        });
+            OutputStream out = conn.getOutputStream();
+            out.write(body);
+            out.close();
+        }
+
+        InputStream in = conn.getInputStream();
+        byte[] buffer = new byte[4096];
+
+        //noinspection StatementWithEmptyBody
+        while (in.read(buffer) != -1) {
+            // discard
+        }
+        in.close();
 
         long stop = System.nanoTime();
         long duration_nanos = stop - start;
         long millis = TimeUnit.MILLISECONDS.convert(duration_nanos, TimeUnit.NANOSECONDS);
-        return new Poll(url, millis);
+        return new Poll(millis);
     }
 }
--- src/main/java/com/ning/pummel/Poll.java
+++ src/main/java/com/ning/pummel/Poll.java
@@ -2,18 +2,11 @@ package com.ning.pummel;
 
 public class Poll
 {
-    private final String url;
     private final long time;
 
-    public Poll(String url, long time)
+    public Poll(long time)
     {
-        this.url = url;
         this.time = time;
-    }
-
-    public String getUrl()
-    {
-        return url;
     }
 
     public long getTime()
